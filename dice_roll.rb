1fee2b6da0a97d8baf9dbc06026bf4e03b36fd02
Ahmadali <ahmadali.azizov@outlook.com>
2018-07-18 14:34:54 +0000

    automatically backed up by learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -1,9 +1,7 @@
-require 'pry'
 # Create method `roll` that returns a random number between 1 and 6
 
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
  rand(1..6)
- binding.pry
 end
